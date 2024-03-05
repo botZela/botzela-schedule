6650467ff188162efccb98bd6295dae469561efb
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-03-05 11:02:38 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -17,157 +17,166 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
 
-        inherit (pkgs) lib;
+      inherit (pkgs) lib;
 
-        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
-        # When filtering sources, we want to allow assets other than .rs files
-        src = lib.cleanSourceWith {
-          src = craneLib.path ./.; # The original, unfiltered source
-          filter = path: type:
-            (lib.hasSuffix "\.html" path) ||
-            (lib.hasSuffix "\.lock" path) ||
-            (lib.hasSuffix "\.scss" path) ||
-            (lib.hasSuffix "\.css" path) ||
-            # Example of a folder for images, icons, etc
-            (lib.hasInfix "/assets/" path) ||
-            (lib.hasInfix "/public/" path) ||
-            # Default filter from crane (allow .rs files)
-            (craneLib.filterCargoSources path type)
-          ;
-        };
-        # src = craneLib.cleanCargoSource (craneLib.path ./.);
+      # When filtering sources, we want to allow assets other than .rs files
+      src = lib.cleanSourceWith {
+        src = craneLib.path ./.; # The original, unfiltered source
+        filter = path: type:
+          (lib.hasSuffix "\.html" path)
+          || (lib.hasSuffix "\.lock" path)
+          || (lib.hasSuffix "\.scss" path)
+          || (lib.hasSuffix "\.css" path)
+          ||
+          # Example of a folder for images, icons, etc
+          (lib.hasInfix "/assets/" path)
+          || (lib.hasInfix "/public/" path)
+          ||
+          # Default filter from crane (allow .rs files)
+          (craneLib.filterCargoSources path type);
+      };
+      # src = craneLib.cleanCargoSource (craneLib.path ./.);
 
+      # Arguments to be used by both the frontend and the server
+      # When building a workspace with crane, it's a good idea
+      # to set "pname" and "version".
+      commonArgs = {
+        inherit src;
+        pname = "botzela-schedule";
+        version = "0.1.0";
+      };
 
+      # Native packages
 
-        # Arguments to be used by both the frontend and the server
-        # When building a workspace with crane, it's a good idea
-        # to set "pname" and "version".
-        commonArgs = {
-          inherit src;
-          pname = "botzela-schedule";
-          version = "0.1.0";
-        };
-
-        # Native packages
-
-        nativeArgs = commonArgs // {
+      nativeArgs =
+        commonArgs
+        // {
           pname = "backend";
         };
 
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly nativeArgs;
+      # Build *just* the cargo dependencies, so we can reuse
+      # all of that work (e.g. via cachix) when running in CI
+      cargoArtifacts = craneLib.buildDepsOnly nativeArgs;
 
-        # Simple JSON API that can be queried by the frontend
-        backend = craneLib.buildPackage (nativeArgs // {
+      # Simple JSON API that can be queried by the frontend
+      backend = craneLib.buildPackage (nativeArgs
+        // {
           inherit cargoArtifacts;
           CLIENT_DIST = frontend;
         });
 
-        # Wasm packages
+      # Wasm packages
 
-        # it's not possible to build the server on the
-        # wasm32 target, so we only build the frontend.
-        wasmArgs = commonArgs // {
+      # it's not possible to build the server on the
+      # wasm32 target, so we only build the frontend.
+      wasmArgs =
+        commonArgs
+        // {
           pname = "botzela-schedule-wasm";
           cargoExtraArgs = "--package=front_schedule_leptos";
           CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
         };
 
-        cargoArtifactsWasm = craneLib.buildDepsOnly (wasmArgs // {
+      cargoArtifactsWasm = craneLib.buildDepsOnly (wasmArgs
+        // {
           doCheck = false;
         });
 
-        # Build the frontend of the application.
-        # This derivation is a directory you can put on a webserver.
-        frontend = craneLib.buildTrunkPackage (wasmArgs // {
+      # Build the frontend of the application.
+      # This derivation is a directory you can put on a webserver.
+      frontend = craneLib.buildTrunkPackage (wasmArgs
+        // {
           pname = "front_schedule_leptos";
           cargoArtifacts = cargoArtifactsWasm;
           trunkIndexPath = "frontend/index.html";
-          nativeBuildInputs = [
-            (pkgs.wasm-bindgen-cli.override {
-              version = "0.2.87";
-              hash = "sha256-0u9bl+FkXEK2b54n7/l9JOCtKo+pb42GF9E1EnAUQa0=";
-              cargoHash = "sha256-AsZBtE2qHJqQtuCt/wCAgOoxYMfvDh8IzBPAOkYSYko=";
-            })
-          ];
+          wasm-bindgen-cli = pkgs.wasm-bindgen-cli.override {
+            version = "0.2.87";
+            hash = "sha256-0u9bl+FkXEK2b54n7/l9JOCtKo+pb42GF9E1EnAUQa0=";
+            cargoHash = "sha256-AsZBtE2qHJqQtuCt/wCAgOoxYMfvDh8IzBPAOkYSYko=";
+          };
         });
 
-        # Create Docker Image with Nix
-        dockerImage = pkgs.dockerTools.streamLayeredImage {
-          name = "botzela-schedule-nix";
-          tag = "latest";
-          config = {
-            Cmd = [ "${backend}/bin/backend" ];
-            Env = [ "CLIENT_DIST=${frontend}" ];
-          };
+      # Create Docker Image with Nix
+      dockerImage = pkgs.dockerTools.streamLayeredImage {
+        name = "botzela-schedule-nix";
+        tag = "latest";
+        config = {
+          Cmd = ["${backend}/bin/backend"];
+          Env = ["CLIENT_DIST=${frontend}"];
         };
-      in
-      {
-        checks = {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit backend frontend;
+      };
+    in {
+      checks = {
+        # Build the crate as part of `nix flake check` for convenience
+        inherit backend frontend;
 
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, reusing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          my-app-clippy = craneLib.cargoClippy (commonArgs // {
+        # Run clippy (and deny all warnings) on the crate source,
+        # again, reusing the dependency artifacts from above.
+        #
+        # Note that this is done as a separate derivation so that
+        # we can block the CI if there are issues here, but not
+        # prevent downstream consumers from building our crate by itself.
+        my-app-clippy = craneLib.cargoClippy (commonArgs
+          // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- --deny warnings";
             # Here we don't care about serving the frontend
             CLIENT_DIST = "";
           });
 
-          # Check formatting
-          my-app-fmt = craneLib.cargoFmt commonArgs;
-        };
+        # Check formatting
+        my-app-fmt = craneLib.cargoFmt commonArgs;
+      };
 
-        apps.default = flake-utils.lib.mkApp {
-          name = "backend";
-          drv = backend;
-        };
+      apps.default = flake-utils.lib.mkApp {
+        name = "backend";
+        drv = backend;
+      };
 
-        packages =
-          {
-            # that way we can build `bin` specifically,
-            # but it's also the default.
-            inherit backend dockerImage;
-            default = backend;
-          };
+      packages = {
+        # that way we can build `bin` specifically,
+        # but it's also the default.
+        inherit backend dockerImage;
+        default = backend;
+      };
 
-        devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues self.checks;
+      devShells.default = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.checks;
 
-          shellHook = ''
-            export CLIENT_DIST=$PWD/frontend/dist;
-          '';
+        shellHook = ''
+          export CLIENT_DIST=$PWD/frontend/dist;
+        '';
 
-          # Extra inputs can be added here
-          nativeBuildInputs = with pkgs; [
-            trunk
-            rustToolchain
-            dive
-            flyctl
-            just
-            cargo-edit
-            rust-analyzer
-            leptosfmt
-          ];
-        };
-
-      });
+        # Extra inputs can be added here
+        nativeBuildInputs = with pkgs; [
+          trunk
+          rustToolchain
+          dive
+          flyctl
+          just
+          cargo-edit
+          cargo-audit
+          rust-analyzer
+          leptosfmt
+        ];
+      };
+    });
 }
