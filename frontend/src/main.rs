7b309c12f7403682fee1ec086aa2f1dcdcd4e248
yassineibr <75940849+yassineibr@users.noreply.github.com>
2024-05-26 23:43:20 +0100

    fix: add panic hook to wasm app

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,5 +2,7 @@ use front_schedule_leptos::*;
 use leptos::*;
 
 fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
     mount_to_body(|| view! { <App/> })
 }
